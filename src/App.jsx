849bd5cf4b67e75a50003ab126d29d2847f52e35
silvak <silvak.jeg@gmail.com>
2024-01-04 17:37:35 -0400

    project init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
+}
+
+export default App;
